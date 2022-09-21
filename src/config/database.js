7ce02a57d9484cf676cb892d0ae8f2a3155fb126
Gsurica <95233509+Gsurica@users.noreply.github.com>
2022-09-20 21:34:18 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'mysql',
   host: 'localhost',
-  username: 'root',
-  password: '161149M@e',
+  username: '',
+  password: '',
   database: 'tasklist',
   define: {
     timestamps: false,
